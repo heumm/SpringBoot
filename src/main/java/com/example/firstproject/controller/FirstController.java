8b5bb2df33c59b75e473cda6d204a19740b90553
heumm <hanheum97@gmail.com>
2021-04-22 17:24:25 +0900

    04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links04.22 activate navbar links

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,22 @@
 package com.example.firstproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
-
+@Slf4j
 public class FirstController {
+
+    @GetMapping("/")
+    public RedirectView home() {
+        log.info("home controller home()메소드 호출!");
+        return new RedirectView("/hi");
+    }
+
 
     @GetMapping("/hi")
     public String niceToMeetyou(Model model) {
