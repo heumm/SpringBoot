e7900af149b01ca88e6672c2f7abd1d2d42800ab
heumm <hanheum97@gmail.com>
2021-06-28 00:46:25 +0900

    06.28

--- src/frontend/src/views/Board.vue
+++ src/frontend/src/views/Board.vue
@@ -3,7 +3,7 @@
         <h1>Board</h1>
         <table>
             <tr>
-                <td></td>
+                <td>{{usergit}}</td>
             </tr>
 
         </table>
@@ -13,12 +13,33 @@
 
 
 <script>
-import usergitdata from 'https://api.github.com/users/heumm'
+// import usergitdata from '@/assets/gitdata.js'
 
+
+// import Vue from 'https://unpkg.com/vue'
+// url (required), options (optional)
+// fetch('https://api.github.com/users/heumm', {
+//     method: 'get'
+// }).then(function(response) {
+//     // console.log(response)
+//     response.json()
+// }).then((data) => console.log(data))
+// .catch(function(err) {
+//     // Error :(
+//         console.log("error:(")
+// });
+fetch('https://api.github.com/users/heumm', {
+    method: 'get'
+}).then((response) => response.json())
+.then((data) => console.log(data))
+.catch(function(err) {
+    // Error :(
+        console.log("error:(")
+});
+ 
 export default {
     data() {
         return {
-            usergit : usergitdata
         }
     },
 }
