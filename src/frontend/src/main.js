b79a5cfe48e830d0874cf798952544acd855f502
heumm <hanheum97@gmail.com>
2021-07-14 22:03:26 +0900

    axios not working

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.provide('$axios', axios);
+
+app.use(VueAxios, axios)
+app.use(router)
+app.mount('#app')
